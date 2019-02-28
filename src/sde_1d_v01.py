f2728745cf4b9ceddf4a78aafc0a811148916595
Zhiyuan She <46581069+ZhyShe@users.noreply.github.com>
2019-02-28 04:50:32 -0500

    Add files via upload

--- src/sde_1d_v01.py
+++ src/sde_1d_v01.py
@@ -3,6 +3,14 @@ import matplotlib.pyplot as plt
 import scipy.stats as ss
 from contract_v01 import VanillaOption
 
+def BM_gen(T1, T2, num):
+    t=np.linspace(T1,T2,num+1)
+    W=np.zeros(num+1)
+    for i in range(num):
+        W[i+1]=W[i]+np.sqrt((T2-T1)/num)*np.random.normal(0,1)
+    return W, t
+    
+    
 class Sde_1d:
     def __init__(self, init_state=0., drift=lambda x:0, vol=lambda x:1, drift_ratio = .0475, vol_ratio = .2):
         self.init_state = init_state
@@ -60,10 +68,29 @@ class Gbm_1d(Sde_1d):
         sigma_hat=np.sqrt(sigma_hat_s)
         if otype==1:
             option=VanillaOption(otype = 1, strike = K, maturity= T, market_price=15.)
-            return float(np.exp([(r_hat-r)*T])*Gbm_1d(init_state=S0, drift_ratio=r_hat, vol_ratio=sigma_hat).bsm_price(option))
+            return np.exp([(r_hat-r)*T])*Gbm_1d(init_state=S0, drift_ratio=r_hat, vol_ratio=sigma_hat).bsm_price(option)
         else:
             option=VanillaOption(otype = -1, strike = K, maturity= T, market_price=15.)
-            return float(np.exp([(r_hat-r)*T])*Gbm_1d(init_state=S0, drift_ratio=r_hat, vol_ratio=sigma_hat).bsm_price(option))
+            return np.exp([(r_hat-r)*T])*Gbm_1d(init_state=S0, drift_ratio=r_hat, vol_ratio=sigma_hat).bsm_price(option)
+    def S_price(self, w, t):
+        return self.init_state*np.exp((self.drift_ratio-0.5*self.vol_ratio**2)*t+self.vol_ratio*w)
+    def bsm_arithmetic_asian_exact_sample(self, otype, strike, maturity, num_step, num_path):
+        W=[]
+        S=[]
+        Mean=[]
+        for i in range(num_path):
+            w, T=BM_gen(0, maturity, num_step)
+            W.append(w)
+        for w in W:
+            S.append(self.S_price(w,T))
+        Means=np.array([np.mean(s) for s in S])-strike
+        for mean in Means:
+            Mean.append(max(mean,0)*np.exp(-self.drift_ratio*maturity))
+        Mean=sum(Mean)/num_path
+        return Mean
+             
+             
+         
             
 
 if __name__ == '__main__':
